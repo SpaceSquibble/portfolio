acdb4307fe43f59eceaff0f569f7a5deda26a1ab
SpaceSquibble <54996465+SpaceSquibble@users.noreply.github.com>
2020-01-27 23:12:44 -0500

    Create house.js

---
+++ java/house.js
@@ -0,0 +1,30 @@
+new p5();
+
+function draw(){
+    background(182, 250, 250);
+
+    //roof
+    fill(174, 180, 214);
+    triangle(200, 28, 350, 150, 50, 150);
+
+    //house
+    fill(255, 255, 255);
+    rect(60, 150, 280, 207);
+    for (var q=60;q<327;q+=70)
+    {
+        for(var p=145;p<370;p+=44)
+        {
+            image(getImage("cute/StoneBlockTall"),q,p,70,96);
+        }
+    }
+
+    //windows
+    for (var x=85;x<283;x+=90)
+    {
+        for(var y=172;y<307;y+=86)
+        {
+            fill(191, 238, 255);
+            rect(x, y, 50, 50);
+        }
+    }
+}
